7b47b02f04c1e9880f3d629efc8e6a9677beb6e8
Socheat Sok <socheatsok78@gmail.com>
2025-09-03 15:14:14 +0700

    Update "font-khmeros-bundle" cask

--- Casks/font-khmeros-bundle.rb
+++ Casks/font-khmeros-bundle.rb
@@ -1,21 +1,21 @@
-cask 'font-khmeros-bundle' do
-  version '5.0.0'
-  sha256 'b597d25b66a83a4f3e252a153b42a4153f704fc5e1fa5fde239bc17679369a73'
+cask "font-khmeros-bundle" do
+  version "5.0.0"
+  sha256 "b597d25b66a83a4f3e252a153b42a4153f704fc5e1fa5fde239bc17679369a73"
 
   url "https://github.com/KhmerSoftwareInitiative/khmer-unicode-fonts/releases/download/v#{version}/khmer-unicode-fonts-v#{version}.zip"
-  name 'KhmerOS Bundle'
-  homepage 'https://github.com/KhmerSoftwareInitiative'
+  name "KhmerOS Bundle"
+  homepage "https://github.com/KhmerSoftwareInitiative"
 
-  font 'KhmerOS.ttf'
-  font 'KhmerOS_battambang.ttf'
-  font 'KhmerOS_bokor.ttf'
-  font 'KhmerOS_content.ttf'
-  font 'KhmerOS_fasthand.ttf'
-  font 'KhmerOS_freehand.ttf'
-  font 'KhmerOS_metalchrieng.ttf'
-  font 'KhmerOS_muol.ttf'
-  font 'KhmerOS_muollight.ttf'
-  font 'KhmerOS_muolpali.ttf'
-  font 'KhmerOS_siemreap.ttf'
-  font 'KhmerOS_sys.ttf'
+  font "KhmerOS_battambang.ttf"
+  font "KhmerOS_bokor.ttf"
+  font "KhmerOS_content.ttf"
+  font "KhmerOS_fasthand.ttf"
+  font "KhmerOS_freehand.ttf"
+  font "KhmerOS_metalchrieng.ttf"
+  font "KhmerOS_muol.ttf"
+  font "KhmerOS_muollight.ttf"
+  font "KhmerOS_muolpali.ttf"
+  font "KhmerOS_siemreap.ttf"
+  font "KhmerOS_sys.ttf"
+  font "KhmerOS.ttf"
 end
