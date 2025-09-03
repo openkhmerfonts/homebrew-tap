ec6fe497accab2d44bf6abd44ebe53200ef1dd02
Socheat Sok <socheatsok78@gmail.com>
2025-09-03 15:52:05 +0700

    Update font-khmeros-bundle & font-open-khmer-school casks
    # Conflicts:
    #	Casks/font-khmeros-bundle.rb

--- Casks/font-khmeros-bundle.rb
+++ Casks/font-khmeros-bundle.rb
@@ -1,21 +1,21 @@
 cask "font-khmeros-bundle" do
   version "5.0.0"
-  sha256 "b597d25b66a83a4f3e252a153b42a4153f704fc5e1fa5fde239bc17679369a73"
+  sha256 "c6fb1777de25c88254c36d1c8bf2f5b0671364cc307f32e6f5dd34abb7942377"
 
-  url "https://github.com/KhmerSoftwareInitiative/khmer-unicode-fonts/releases/download/v#{version}/khmer-unicode-fonts-v#{version}.zip"
+  url "https://github.com/KhmerSoftwareInitiative/khmer-unicode-fonts/releases/download/v#{version}/khmer-unicode-fonts-v#{version}.tar.gz"
   name "KhmerOS Bundle"
   homepage "https://github.com/KhmerSoftwareInitiative"
 
-  font "KhmerOS_battambang.ttf"
-  font "KhmerOS_bokor.ttf"
-  font "KhmerOS_content.ttf"
-  font "KhmerOS_fasthand.ttf"
-  font "KhmerOS_freehand.ttf"
-  font "KhmerOS_metalchrieng.ttf"
-  font "KhmerOS_muol.ttf"
-  font "KhmerOS_muollight.ttf"
-  font "KhmerOS_muolpali.ttf"
-  font "KhmerOS_siemreap.ttf"
-  font "KhmerOS_sys.ttf"
-  font "KhmerOS.ttf"
+  font 'fonts/v5.0/KhmerOS.ttf'
+  font 'fonts/v5.0/KhmerOS_battambang.ttf'
+  font 'fonts/v5.0/KhmerOS_bokor.ttf'
+  font 'fonts/v5.0/KhmerOS_content.ttf'
+  font 'fonts/v5.0/KhmerOS_fasthand.ttf'
+  font 'fonts/v5.0/KhmerOS_freehand.ttf'
+  font 'fonts/v5.0/KhmerOS_metalchrieng.ttf'
+  font 'fonts/v5.0/KhmerOS_muol.ttf'
+  font 'fonts/v5.0/KhmerOS_muollight.ttf'
+  font 'fonts/v5.0/KhmerOS_muolpali.ttf'
+  font 'fonts/v5.0/KhmerOS_siemreap.ttf'
+  font 'fonts/v5.0/KhmerOS_sys.ttf'
 end
--- Casks/font-open-khmer-school.rb
+++ Casks/font-open-khmer-school.rb
@@ -1,8 +1,8 @@
 cask "font-open-khmer-school" do
   version "1.421"
-  sha256 :no_check
+  sha256 "1409284b87bcc95bb11b4b1ddb266a940756591a566a647241e7659bc576db66"
 
-  url "https://github.com/OpenInstituteCambodia/open-khmer-school/archive/refs/tags/v#{version}.zip"
+  url "https://github.com/OpenInstituteCambodia/open-khmer-school/archive/refs/tags/v#{version}.tar.gz"
   name "Open Khmer School"
   homepage "https://github.com/OpenInstituteCambodia/open-khmer-school"
 
